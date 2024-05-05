f9f1d1901f1d969ceb6105dc2b01c2aade36477e
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-05-05 13:35:30 +0530

    chore: redirect to home page from navbar

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
       <div className='flex items-center gap-8'>
         <h1>Logo</h1>
         <div className='md:flex items-center gap-6 hidden'>
-          <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Home</h2>
+          <Link href={'/'}><h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Home</h2></Link>
           <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Services</h2>
           <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>About us</h2>
         </div>
