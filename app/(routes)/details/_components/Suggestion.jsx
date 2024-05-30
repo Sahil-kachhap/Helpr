97dfb917528a9f83d2f16bb3402561b87ac347c6
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-05-30 21:30:58 +0530

    fixed: suggestions show multiple profile with same data

--- app/(routes)/details/_components/Suggestion.jsx
+++ app/(routes)/details/_components/Suggestion.jsx
@@ -32,12 +32,12 @@ function Suggestion({buisness}) {
       <h2 className='mt-3 text-lg font-bold mb-3'>Similar Buisness</h2>
       <div>
         {buisnessList && buisnessList.map((item, index)=>(
-          <Link href={'/details/'+buisness.id} className='flex gap-2 mb-4 hover:border border-primary rounded-lg p-2 cursor-pointer hover:shadow-md'>
+          <Link href={'/details/'+item.id} className='flex gap-2 mb-4 hover:border border-primary rounded-lg p-2 cursor-pointer hover:shadow-md'>
             <Image src={item.images[0].url} width={80} height={80} alt={item.name} key={index} className='rounded-lg object-cover'/>
             <div>
-              <h2 className='font-bold'>{buisness.name}</h2>
-              <h2 className='text-primary'>{buisness.contactPerson}</h2>
-              <h2 className='text-gray-400'>{buisness.address}</h2>
+              <h2 className='font-bold'>{item.name}</h2>
+              <h2 className='text-primary'>{item.contactPerson}</h2>
+              <h2 className='text-gray-400'>{item.address}</h2>
             </div>
           </Link>
         ))}
